f5104f2d1c4c71d72db755511464c47b01513522
TimothyPC <42684805+TimothyPC@users.noreply.github.com>
2021-01-12 11:37:46 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -79,15 +79,17 @@ function toggle_info_2() {
 
 
 
-const WebSocket = require('ws');
-const ws = new WebSocket.Server({ port: 8080 });
+const socket = new WebSocket('ws://localhost:8080'); 
+socket.addEventListener('open', function (event) { 
+  socket.send('Hello Server!'); 
+}); 
 
-ws.on('connection', function connection(wsConnection) {
-  wsConnection.on('message', function incoming(message) {
-    console.log(`server received: ${message}`);
-  });
+socket.addEventListener('message', function (event) { 
+  console.log('Message from server ', event.data); 
+});
 
-  wsConnection.send('got your message!');
+socket.addEventListener('close', function (event) { 
+  console.log('The connection has been closed'); 
 });
 
 
